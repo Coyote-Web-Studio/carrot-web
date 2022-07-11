3323d99b95331556374efecba137e527aa6fa24e
guidolarosa <guido.glarosa@gmail.com>
2022-07-11 00:55:04 -0300

    Set lightTheme to default

--- context/theme.tsx
+++ context/theme.tsx
@@ -6,7 +6,7 @@ import darkTheme from './../themes/dark';
 const Context : any = createContext('');
 
 const ThemeContext = ({children} : any) => {
-    const [theme, setTheme] = useState(darkTheme);
+    const [theme, setTheme] = useState(lightTheme);
 
     return (
         <Context.Provider value={[theme, setTheme]}>
