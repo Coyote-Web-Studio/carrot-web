2b0362a46702923c13e80742dd6aa1f57e3686cf
luzzifoss <fedeluzzi00@gmail.com>
2023-10-10 10:02:10 +0100

    feat: add max width to how it works section

--- components/homepage/HowItWorks.tsx
+++ components/homepage/HowItWorks.tsx
@@ -6,23 +6,25 @@ const HowItWorks = () => {
     return (
         <div
             id="how-it-works"
-            className="px-4 md:px-10 lg:px-14 xl:px-40 py-36 flex flex-col gap-20"
+            className="px-4 md:px-10 lg:px-14 xl:px-40 py-36 flex flex-col items-center"
         >
-            <Typography
-                variant="h2"
-                className={{
-                    root: "text-[5rem] md:text-[6rem] lg:text-[7.4rem]",
-                }}
-                data-aos="fade-up"
-            >
-                How it works
-            </Typography>
-            <div className="flex flex-col">
-                {HOW_IT_WORKS_CONTENT.map((contentBlock, index) => (
-                    <div key={index}>
-                        <HowItWorksBlock index={index} {...contentBlock} />
-                    </div>
-                ))}
+            <div className="max-w-screen-2xl flex flex-col gap-20">
+                <Typography
+                    variant="h2"
+                    className={{
+                        root: "text-[5rem] md:text-[6rem] lg:text-[7.4rem]",
+                    }}
+                    data-aos="fade-up"
+                >
+                    How it works
+                </Typography>
+                <div className="flex flex-col">
+                    {HOW_IT_WORKS_CONTENT.map((contentBlock, index) => (
+                        <div key={index}>
+                            <HowItWorksBlock index={index} {...contentBlock} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
